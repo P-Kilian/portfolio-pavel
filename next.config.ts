d6171be7a9e399076dc6ec68a8eb247b2e6f7442
Pavel Kilian <pavel.k.i.pp@hotmail.com>
2024-12-06 05:12:03 +0100

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: "/2048-in-react",
-  output: "export",
   reactStrictMode: true,
+  dependencies: {
+    react: "19.0.0-rc-66855b96-20241106",
+    "react-dom": "19.0.0-rc-66855b96-20241106",
+    next: "15.0.3",
+  },
 };
 
 export default nextConfig;
