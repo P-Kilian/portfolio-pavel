a4f7b98cb8d6d2d4d46bc145adcae38e64c3fef0
Pavel Kilian <pavel.k.i.pp@hotmail.com>
2024-12-06 05:08:18 +0100

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { IBM_Plex_Sans, Poppins } from "next/font/google";
 import "./globals.css";
 
+import "./fonts/GeistMonoVF.woff";
+import "./fonts/GeistVF.woff";
+
 const ibmPlex = IBM_Plex_Sans({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700"],
